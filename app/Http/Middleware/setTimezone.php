af6f4ce42944172300b8975fea171a950f27e23a
Vinicius Bassalobre <=>
2020-12-10 14:54:49 -0300

    corrige settimezone

--- app/Http/Middleware/setTimezone.php
+++ app/Http/Middleware/setTimezone.php
@@ -8,30 +8,22 @@ use DB;
 
 class setTimezone
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $user = Auth::user();
-        if ($tenant = @$user->tenant) {
-            $timezone =  @$tenant->timezone ? $tenant->timezone : "UTC";
-            DB::disconnect('mysql');
-            config(['app.timezone' => $timezone]);
-            date_default_timezone_set($timezone);
-            $this->setDbTimezone($timezone);
-        }
-        return $next($request);
-    }
-
-    private function setDbTimezone($timezone)
-    {
-        $_timezone = @config("timezones")[$timezone];
-        $_timezone = $_timezone ? $_timezone :  "+00:00";
-        DB::select(DB::raw("SET time_zone = '$_timezone'"));
-    }
+	/**
+	 * Handle an incoming request.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  \Closure  $next
+	 * @return mixed
+	 */
+	public function handle($request, Closure $next)
+	{
+		$user = Auth::user();
+		if ($tenant = @$user->tenant) {
+			$timezone =  @$tenant->timezone ? $tenant->timezone : "UTC";
+			DB::disconnect('mysql');
+			config(['app.timezone' => $timezone]);
+			date_default_timezone_set($timezone);
+		}
+		return $next($request);
+	}
 }
