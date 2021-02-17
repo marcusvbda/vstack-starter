f974c7dc032528152bb2fc26c15d5c3b88d498d2
Vinicius Bassalobre <=>
2021-02-17 10:31:01 -0300

    roles

--- app/Http/Controllers/RolesController.php
+++ app/Http/Controllers/RolesController.php
@@ -10,14 +10,6 @@ use Cache;
 
 class RolesController extends Controller
 {
-	public function create()
-	{
-		$resource = ResourcesHelpers::find("grupos-de-acesso");
-		if (!$resource->canCreate()) abort(401);
-		$permissions = $this->getGroupedPermissions();
-		return view("admin.roles.create", compact("resource", "permissions"));
-	}
-
 	private function getGroupedPermissions()
 	{
 		$groups = Permission::orderBy("description")->groupBy("group")->pluck("group")->toArray();
@@ -49,14 +41,5 @@ class RolesController extends Controller
 		Cache::flush('spatie.permission.cache');
 		Cache::flush('spatie.role.cache');
 		return ["success" => true, "route" => route('resource.index', ["resource" => $resource->id])];
-	}
-
-	public function edit($id)
-	{
-		$resource = ResourcesHelpers::find("grupos-de-acesso");
-		if (!$resource->canUpdate()) abort(401);
-		$role = $resource->model->findOrFail($id);
-		$permissions = $this->getGroupedPermissions();
-		return view("admin.roles.edit", compact("resource", "permissions", "role"));
 	}
 }
